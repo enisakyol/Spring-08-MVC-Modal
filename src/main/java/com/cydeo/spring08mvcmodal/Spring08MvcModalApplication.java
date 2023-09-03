730b50c203ab6a3695156975158122dda6a9de22
enis <enis_akyol@yahoo.com>
2023-09-03 21:13:16 +0300

    MVC-Modal

---
+++ src/main/java/com/cydeo/spring08mvcmodal/Spring08MvcModalApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring08mvcmodal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring08MvcModalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring08MvcModalApplication.class, args);
+    }
+
+}
